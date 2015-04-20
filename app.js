c76a089f1cd91b2ffc944f6ba4f2631269ee253d
Sebastian Müller <me@sibbl.net>
2015-04-20 21:36:24 +0200

    get temperature from forecast.io

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 ﻿var express = require('express'),
+    q = require('q'),
+    request = require('request'),
     svgexport = require('svgexport'),
     fs = require('fs'),
     path = require('path'),
@@ -12,11 +14,20 @@ var filenames = {
     'pngdestination': 'cover.png'
 }
 
-var generateVars = function() {
-    return {
-        'time': moment.tz('Europe/Berlin').format('D.MM.YYYY HH:mm'),
-        'temp': 5
-    };
+var generateVars = function () {
+    var def = q.defer();
+    request("https://api.forecast.io/forecast/d382a86ed6a0677340258ab891d8097c/51.0211287,13.7511783", function (error, response, body) {
+        if (error || response.statusCode != 200) {
+            def.reject('something bad happened');
+        } else {
+            def.resolve({
+                'time': moment.tz('Europe/Berlin').format('D.MM.YYYY HH:mm'),
+                'temp': parseInt((JSON.parse(body).currently.temperature - 32) * 5 / 9)
+            });
+        }
+    });
+    
+    return def.promise;
 }
 
 var app = express();
@@ -25,36 +36,36 @@ app.set('port', (process.env.PORT || defaultPort));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-
     fs.readFile(filenames.svgsource, 'utf8', function(err, data) {
         if (err) {
             return console.log(err);
         }
-        var result = data;
-        var replaceVars = generateVars();
-        for (var i in replaceVars) {
-            result = result.replace(new RegExp('%' + i + '%', 'g'), replaceVars[i]);
-        }
+        generateVars().then(function(replaceVars) {
+            var result = data;
+            for (var i in replaceVars) {
+                result = result.replace(new RegExp('%' + i + '%', 'g'), replaceVars[i]);
+            }
 
-        fs.writeFile('input_filled.svg', result, 'utf8', function(err) {
-            if (err) return console.log(err);
+            fs.writeFile('input_filled.svg', result, 'utf8', function(err) {
+                if (err) return console.log(err);
 
-            svgexport.render({
-                "input": "input_filled.svg",
-                "output": "converted.png"
-            }, function(a) {
-                if (a == 0) {
-                    gm('converted.png')
-                        .options({ imageMagick: true })
-                        .type('GrayScale')
-                        .bitdepth(8)
-                        .write(filenames.pngdestination, function(err) {
-                            if (err) return console.log(err);
-                            response.status(200).sendFile(path.join(__dirname, filenames.pngdestination));
-                            fs.unlinkSync('converted.png');
-                            fs.unlinkSync('input_filled.svg');
-                        });
-                }
+                svgexport.render({
+                    "input": "input_filled.svg",
+                    "output": "converted.png"
+                }, function(a) {
+                    if (a == 0) {
+                        gm('converted.png')
+                            .options({ imageMagick: true })
+                            .type('GrayScale')
+                            .bitdepth(8)
+                            .write(filenames.pngdestination, function(err) {
+                                if (err) return console.log(err);
+                                response.status(200).sendFile(path.join(__dirname, filenames.pngdestination));
+                                fs.unlinkSync('converted.png');
+                                fs.unlinkSync('input_filled.svg');
+                            });
+                    }
+                });
             });
         });
     });
