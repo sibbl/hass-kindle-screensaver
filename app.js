6ea239dbe9458d330d1811b518cd97a6dcff5e18
Sebastian <me@sibbl.net>
2017-03-30 20:03:56 +0200

    Someone shut down their Netatmo device? Alright, then let's use another one.

--- app.js
+++ app.js
@@ -14,10 +14,10 @@
 var config = {
     defaultLanguage: 'de-DE',
     defaultTimezone: 'Europe/Berlin',
-    netatmoDeviceId: '70:ee:50:1e:0d:d8',
+    netatmoDeviceId: '70:ee:50:04:fd:1a',
     netatmoAuthUrl: 'https://api.netatmo.com/oauth2/token',
     netatmoAuthCredentials: require('./oauthData.json'),
-    netatmoTemperatureModuleId: '02:00:00:1e:05:e0',
+    netatmoTemperatureModuleId: '02:00:00:04:d2:12',
     netatmoHistoryUrl: 'https://www.netatmo.com/api/getmeasure',
     weatherProForecastUrl: 'http://windows.weatherpro.meteogroup.de/weatherpro/WeatherFeed.php?lid=18228265',
     temperatureChartBeginDayTime: { hours: 2 },
